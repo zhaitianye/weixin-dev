1f1d704ad8d729b50f6bbffa7bbac202ec120288
zhaitianye <1048972989@qq.com>
2019-07-05 18:41:45 +0800

    修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,12 @@ module.exports = {
     apps: [{
         name: 'weixinDev',
         script: 'bin/www',
+        watch: false,
+        watch_delay: 1000,
+        ignore_watch : ["node_modules", "data/log",".vscode"],
+        watch_options: {
+        "followSymlinks": false
+        },
         output: "data/log/pm2_nova_out.log",
         error: "data/log/pm2_nova_error.log",
         log_date_format: "DD-MM-YYYY",
